cb3f9b7313c9ac5793d0dc53e2b42d5b85ec1a94
cretto <creto_boy@hotmail.com>
2019-03-24 19:23:48 -0300

    GoNative-desafio1 - Finalizado

--- src/Index.js
+++ src/Index.js
@@ -1,41 +1,108 @@
 import './config/ReactotronConfig';
 import './config/DevToolsConfig';
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  Platform, StyleSheet, Text, View,
+  Platform, StyleSheet, Text, View, ScrollView,
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
-});
+import Post from '~/components/Post';
+
+const white = '#FFF';
+const primary = '#EE7777';
+const title = '#333';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: primary,
+  },
+
+  header: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: white,
+    ...Platform.select({
+      ios: {
+        height: 76,
+        paddingTop: 20,
+      },
+      android: {
+        height: 56,
+        paddingTop: 0,
+      },
+    }),
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  headerTitle: {
+    color: title,
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
 
-const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>Welcome to Test!</Text>
-    <Text style={styles.instructions}>To get started, edit App.js</Text>
-    <Text style={styles.instructions}>{instructions}</Text>
-  </View>
-);
+export default class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'Aprendendo React Native',
+        author: 'Diego Fernandes',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 2,
+        title: 'Aprendendo React Native',
+        author: 'Diego Fernandes',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 3,
+        title: 'Aprendendo React Native',
+        author: 'Diego Fernandes',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 4,
+        title: 'Aprendendo React Native',
+        author: 'Diego Fernandes',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 5,
+        title: 'Aprendendo React Native',
+        author: 'Diego Fernandes',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 6,
+        title: 'Aprendendo React Native',
+        author: 'Diego Fernandes',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+    ],
+  };
 
-export default App;
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>GoNative App</Text>
+        </View>
+        <ScrollView>
+          {posts.map(post => (
+            <Post key={post.id} data={post} />
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
+}
